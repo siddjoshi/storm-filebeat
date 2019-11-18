5acae5ac41c69a975edfc52d80ee5a642c4b365f
Siddharth <siddharth.joshi@bold.com>
2019-11-18 19:13:28 +0530

    changes yaml to yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,13 +8,13 @@ setup=$DEPLOYMENT
 
 
 ## Change Elastic servers
-sed -i 's/change_elastic/'$elastic'/g' /etc/filebeat/filebeat.yaml
+sed -i 's/change_elastic/'$elastic'/g' /etc/filebeat/filebeat.yml
 
 ## Change Username
-sed -i 's/change_username/'$usern'/g' /etc/filebeat/filebeat.yaml
+sed -i 's/change_username/'$usern'/g' /etc/filebeat/filebeat.yml
 
 ## Change Password
-sed -i 's/change_password/'$passwd'/g' /etc/filebeat/filebeat.yaml
+sed -i 's/change_password/'$passwd'/g' /etc/filebeat/filebeat.yml
 
 ##change Environemnt
-sed -i 's/change_environment/'$setup'/g' /etc/filebeat/filebeat.yaml
+sed -i 's/change_environment/'$setup'/g' /etc/filebeat/filebeat.yml
